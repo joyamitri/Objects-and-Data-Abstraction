0527d64485d88abee11ea8c843de211a1ad0194e
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2024-05-08 23:22:24 +0300

    Updating <> Recursion - Sequential Search

--- Recursion/sequential_search.java
+++ Recursion/sequential_search.java
@@ -1,21 +1,23 @@
-// Write a recursive method that performs sequential search on an array
+public class sequential_search {
+    // Write a recursive method that performs sequential search on an array
 
-public boolean search(int[] a, int k, int i){
-    // returns true if k is in a[], false otherwise
-    // i is the index where search should start
+    public boolean search(int[] a, int k, int i){
+        // returns true if k is in a[], false otherwise
+        // i is the index where search should start
 
-    if((i > a.length - 1) || (i < 0)){
-        return false;
-    }
-    else{
-        if(a[i] == k){
-            return true;
-        }
-        else if(i == a.length - 2 && a[i] != k){
+        if((i > a.length - 1) || (i < 0)){
             return false;
         }
         else{
-            search(a, k, i+1);
+            if(a[i] == k){
+                return true;
+            }
+            else if(i == a.length - 2 && a[i] != k){
+                return false;
+            }
+            else{
+                return search(a, k, i+1);
+            }
         }
     }
 }
