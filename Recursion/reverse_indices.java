457e184b68923136569940161024f040da64b4f9
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2024-05-08 11:22:16 +0300

    Creating <> Recursion - Reverse Indices

---
+++ Recursion/reverse_indices.java
@@ -0,0 +1,15 @@
+//  Write a method that reverses an array between 2 indices
+
+public static void reverse(int[] a, int s, int e){
+    int temp = 0;
+
+    if(s >= e){
+        return;
+    }
+
+    temp = a[s];
+    a[s] = a[e];
+    a[e] = temp;
+
+    reverse(a, s + 1, e - 1);
+}
