d09244f969efe416a1fb3591c3ffd076266141b6
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2024-05-08 23:22:34 +0300

    Updating <> Recursion - Sum

--- Recursion/sum.java
+++ Recursion/sum.java
@@ -1,14 +1,16 @@
-// Write a function that calculates the sum from 1 to n recursively 
+public class sum{
+    // Write a function that calculates the sum from 1 to n recursively 
 
-public int sum (int n){
-    if(n == 1){
-        return 1;
+    public static int summ(int n){
+        if(n == 1){
+            return 1;
+        }
+        else{
+            return (n + summ(n - 1));
+        }
     }
-    else{
-        return (n + sum(n - 1));
-    }
-}
 
-public static void main (String[] args){
-    System.out.println(sum(5));
+    public static void main (String[] args){
+        System.out.println(summ(5));
+    }
 }
