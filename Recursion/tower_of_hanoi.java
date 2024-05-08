bfe0056f5f76f10bc642f98b8bfe695d85c5040e
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2024-05-08 11:16:43 +0300

    Creating <> Recursion - Tower of Hanoi

---
+++ Recursion/tower_of_hanoi.java
@@ -0,0 +1,16 @@
+// Write Tower of Hanoi
+
+public void moveOneDisk(int start, int end){
+    System.out.println("Move disk from: " + start + "to" + end);
+}
+
+public void moveTower(int n, int start, int end, int middle){
+    if(n == 1){
+        moveOneDisk(start, end);
+    }
+    else{
+        moveTower(n - 1, start, middle, end);
+        moveOneDisk(start, end);
+        moveTower(n - 1, middle, end, start);
+    }
+}
