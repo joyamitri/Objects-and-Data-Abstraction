7e6ccaeb35fac1c37b7ef2f65da7bcea57c12d87
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2024-05-08 23:21:39 +0300

    Updating <> Recursion - Factorial

--- Recursion/factorial.java
+++ Recursion/factorial.java
@@ -1,17 +1,19 @@
-// Calculate recursively n!
+public class factorial{
+    // Calculate recursively n!
 
-public long fact (int n){
-    if(n == 0){
-        return 1;
+    public static long fact (int n){
+        if(n == 0){
+            return 1;
+        }
+        else if(n == 1){
+            return 1;
+        }
+        else{
+            return(n * fact(n - 1));
+        }
     }
-    else if(n == 1){
-        return 1;
-    }
-    else{
-        return(n * fact(n - 1));
-    }
-}
 
-public static void main (String[] args){
-    System.out.println(fact(3));
+    public static void main (String[] args){
+        System.out.println(fact(3));
+    }
 }
