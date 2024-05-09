577bf0e8b424762514574b630fe9189f6206e342
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2024-05-09 15:07:14 +0300

    Creating <> Linked List

---
+++ HardwareStore/LinkedList.java
@@ -0,0 +1,17 @@
+package HardwareStore;
+
+public class LinkedList {
+    private Node head;
+
+	public LinkedList(Node head) {
+		this.head = head;
+	}
+
+	public Node getHead() {
+		return head;
+	}
+
+	public void setHead(Node head) {
+		this.head = head;
+	}
+}
